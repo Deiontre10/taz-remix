3273622ad8e79a85e34d4a2a0a3e70cdd18b600d
Taz <travontazlowry@gmail.com>
2024-07-01 21:21:54 -0400

    initial commit

--- app/root.tsx
+++ app/root.tsx
@@ -16,7 +16,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <Meta />
         <Links />
       </head>
-      <body>
+      <body className="bg-slate-500">
         {children}
         <ScrollRestoration />
         <Scripts />
