d41723370c0eabc9a9a928156e3b18200616afb2
Taz <travontazlowry@gmail.com>
2024-07-09 11:19:48 -0400

    added my image, and a little framer motion

--- app/components/About.tsx
+++ app/components/About.tsx
@@ -1,6 +1,22 @@
+import { motion } from "framer-motion";
+
+import me from "../../public/me.jpg";
+
 const About = () => {
   return (
     <div className="grid max-w-prose gap-2.5 justify-items-center py-4">
+      <motion.div
+        initial={{ opacity: 0, scale: 0.5 }}
+        animate={{ opacity: 1, scale: 1 }}
+        transition={{ duration: 0.5 }}
+      />
+      <motion.img
+        src={me}
+        className="h-48 w-48 rounded-full object-cover "
+        alt="Your Image"
+        initial="initial"
+        whileHover="hover"
+      />
       <div className="font-bold text-2xl">
         <span className="text-purple-700">T</span>az Lowry.
       </div>
